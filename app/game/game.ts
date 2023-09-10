423ae230f39c257b1f663be81d13e3d50812ae99
윤인성 <yun-inseong@yun-inseong-ui-MacBookAir.local>
2023-09-10 19:31:10 +0900

    re

--- app/game/game.ts
+++ app/game/game.ts
@@ -1,10 +1,36 @@
 import { ByteBuffer } from "../library/byte-buffer";
 import Matter, { Vector } from "matter-js";
 import { RefObject } from "react";
+import { JsxEmit } from "typescript";
+
+function writePhysicsAttribute(payload: ByteBuffer, data: PhysicsAttribute) {
+	payload.write8Float(data.position.x);
+	payload.write8Float(data.position.y);
+	payload.write8Float(data.velocity.x);
+	payload.write8Float(data.velocity.y);
+}
+
+function writeFrame(payload: ByteBuffer, frame: Frame) {
+	payload.write4Unsigned(frame.id);
+	writePhysicsAttribute(payload, frame.paddle1);
+	payload.writeBoolean(frame.paddle1Hit);
+	writePhysicsAttribute(payload, frame.paddle2);
+	payload.writeBoolean(frame.paddle2Hit);
+	writePhysicsAttribute(payload, frame.ball);
+	payload.write4(frame.player1Score);
+	payload.write4(frame.player2Score);
+}
+
+function writeFrames(payload: ByteBuffer, frames: Frame[]) {
+	payload.write4Unsigned(frames.length);
+	for (let i = 0; i < frames.length; i++) {
+		writeFrame(payload, frames[i]);
+	}
+}
 
 function readPhysicsAttribute(payload: ByteBuffer): PhysicsAttribute {
-	const posX = payload.read4();
-	const posY = payload.read4();
+	const posX = payload.read8Float();
+	const posY = payload.read8Float();
 	const velocX = payload.read8Float();
 	const velocY = payload.read8Float();
 	return { position: { x: posX, y: posY }, velocity: { x: velocX, y: velocY } };
@@ -17,8 +43,8 @@ function readFrame(payload: ByteBuffer): Frame {
 	const paddle2 = readPhysicsAttribute(payload);
 	const paddle2Hit = payload.readBoolean();
 	const ball = readPhysicsAttribute(payload);
-	const player1Score = payload.read1();
-	const player2Score = payload.read1();
+	const player1Score = payload.read4();
+	const player2Score = payload.read4();
 	return { id, paddle1, paddle1Hit, paddle2, paddle2Hit, ball, player1Score, player2Score };
 }
 
@@ -29,24 +55,6 @@ function readFrames(payload: ByteBuffer): Frame[] {
 		frames.push(readFrame(payload));
 	}
 	return frames;
-}
-
-function writePhysicsAttribute(payload: ByteBuffer, data: PhysicsAttribute) {
-	payload.write4(data.position.x);
-	payload.write4(data.position.y);
-	payload.write8Float(data.velocity.x);
-	payload.write8Float(data.velocity.y);
-}
-
-function writeFrame(payload: ByteBuffer, frame: Frame) {
-	payload.write4Unsigned(frame.id);
-	writePhysicsAttribute(payload, frame.paddle1);
-	payload.writeBoolean(frame.paddle1Hit);
-	writePhysicsAttribute(payload, frame.paddle2);
-	payload.writeBoolean(frame.paddle2Hit);
-	writePhysicsAttribute(payload, frame.ball);
-	payload.write1(frame.player1Score);
-	payload.write1(frame.player2Score);
 }
 
 export const enum GameServerOpcode {
@@ -186,7 +194,8 @@ export class Game {
 			const buf = ByteBuffer.from(event.data);
 			const opcode = buf.readOpcode();
 			if (opcode === GameClientOpcode.SYNC) {
-				const frame: Frame = readFrame(buf);
+				// const frame: Frame = readFrame(buf);
+				const frame: Frame = JSON.parse(buf.readString())
 				if (this.player === 2) {
 					this.reverseFrame(frame);
 				}
@@ -196,7 +205,8 @@ export class Game {
 				this.drawFrame(frame);
 			}
 			else if (opcode === GameClientOpcode.RESYNC) {
-				const frames: Frame[] = readFrames(buf);
+				// const frames: Frame[] = readFrames(buf);
+				const frames: Frame[] = JSON.parse(buf.readString());
 				const size = frames.length;
 				for (let i = 0; i < size; i++) {
 					if (this.player === 2) {
@@ -555,7 +565,8 @@ export class Game {
 		this.frames.push(frame);
 		const buf = ByteBuffer.createWithOpcode(GameServerOpcode.FRAME);
 		buf.write1(this.player);
-		writeFrame(buf, frame);
+		// writeFrame(buf, frame);
+		buf.writeString(JSON.stringify(frame));
 		this.websocket.send(buf.toArray());
 	}
 
